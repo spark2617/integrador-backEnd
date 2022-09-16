5114bc668b055df178003b218ec5ec60b3d7c9e0
Abe <abrahao.184@gmail.com>
2022-09-16 15:45:38 -0300

    ajustes

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,9 +1,9 @@
 -- Seeding de dados
-INSERT INTO produto (title, image, description, price) VALUES ('Amazon', '', 'Milhões de produtos com ótimos preços. Ofertas em Livros, Eletrônicos, Moda, Games e mais. Ofertas em Eletrônicos, Moda, Livros, Games e mais. Frete Grátis com Prime. Até 10x Sem Juros. Entrega Rápida. Frete Grátis Ilimitado. Serviços: Moda, Livros, Eletrônicos.', '9.99');
-INSERT INTO produto (title, image, description, price) VALUES ('Mercado Livre', '', 'Descontaço no Mercado Livre ➞ Encontre as ✓ MELHORES OFERTAS ✓ em smartphones, moda, beleza e muito mais!', '79');
-INSERT INTO produto (title, image, description, price) VALUES ('Shopee', '', 'Descubra a plataforma de compras on-line Shopee. Compre em menos de 30 segundos: a qualquer hora e em qualquer lugar. Garantia Shopee | Ofertas Relâmpago.', '0.00');
-INSERT INTO produto (title, image, description, price) VALUES ('KaBum', '', 'Encontre as melhores ofertas em Tecnologia e Games aqui no KaBuM!. Confira nossa linha de Hardware, Smartphones, Computadores, Eletrônicos e mais. Acesse!', '56.77');
-INSERT INTO produto (title, image, description, price) VALUES ('Pichau', '', 'Os Melhores Preços do Brasil é na Pichau! Conheça nossa linha de Computadores Gamer, Cadeiras, Monitores, Placas de Vídeo, Processadores e Muito Mais.', '954.23');
+INSERT INTO produto (title, image, description, price) VALUES ('Amazon', '', 'Milhões de produtos com ótimos preços. Ofertas em Livros, Eletrônicos, Moda, Games e mais. Frete Grátis com Prime.', 9.99);
+INSERT INTO produto (title, image, description, price) VALUES ('Mercado Livre', '', 'Descontaço no Mercado Livre ➞ Encontre as ✓ MELHORES OFERTAS ✓ em smartphones, moda, beleza e muito mais!', 79);
+INSERT INTO produto (title, image, description, price) VALUES ('Shopee', '', 'Descubra a plataforma de compras on-line Shopee. Compre em menos de 30 segundos: a qualquer hora e em qualquer lugar. Garantia Shopee | Ofertas Relâmpago.', 0.00);
+INSERT INTO produto (title, image, description, price) VALUES ('KaBum', '', 'Encontre as melhores ofertas em Tecnologia e Games aqui no KaBuM!. Confira nossa linha de Hardware, Smartphones, Computadores, Eletrônicos e mais. Acesse!', 56.77);
+INSERT INTO produto (title, image, description, price) VALUES ('Pichau', '', 'Os Melhores Preços do Brasil é na Pichau! Conheça nossa linha de Computadores Gamer, Cadeiras, Monitores, Placas de Vídeo, Processadores e Muito Mais.', 954.23);
 
 INSERT INTO categoria (name) VALUES ('Loja da boa');
 INSERT INTO categoria (name) VALUES ('Gamer');
@@ -12,11 +12,11 @@ INSERT INTO categoria (name) VALUES ('Mercadola Web');
 INSERT INTO categoria (name) VALUES ('Tipo Magalu');
 INSERT INTO categoria (name) VALUES ('Pique o Brás');
 
--- Precisa criar uma terceira tabela para o @manyToMany???
 
-INSERT INTO produto_categoria (produto_id, categoria_id) VALUE (1, 5);
-INSERT INTO produto_categoria (produto_id, categoria_id) VALUE (2, 5);
-INSERT INTO produto_categoria (produto_id, categoria_id) VALUE (3, 6);
-INSERT INTO produto_categoria (produto_id, categoria_id) VALUE (4, 3);
-INSERT INTO produto_categoria (produto_id, categoria_id) VALUE (5, 2);
+INSERT INTO categoria_produto (categoria_id,produto_id) VALUE (1, 1);
+INSERT INTO categoria_produto (categoria_id,produto_id) VALUE (2, 1);
+INSERT INTO categoria_produto (categoria_id,produto_id) VALUE (3, 2);
+INSERT INTO categoria_produto (categoria_id,produto_id) VALUE (4, 3);
+INSERT INTO categoria_produto (categoria_id,produto_id) VALUE (5, 4);
+INSERT INTO categoria_produto (categoria_id,produto_id) VALUE (6, 4);
 
