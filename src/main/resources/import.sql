a25e035afd50323af19f304f18022bf40a950a37
Vinícius Marques <58312849+marqvin@users.noreply.github.com>
2022-09-16 05:53:25 -0300

    seeding produtos

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,7 @@
+-- Seeding de dados
+INSERT INTO produto (title, image, description, price) VALUES ('Amazon', '', 'Milhões de produtos com ótimos preços. Ofertas em Livros, Eletrônicos, Moda, Games e mais. Ofertas em Eletrônicos, Moda, Livros, Games e mais. Frete Grátis com Prime. Até 10x Sem Juros. Entrega Rápida. Frete Grátis Ilimitado. Serviços: Moda, Livros, Eletrônicos.', '9.99');
+INSERT INTO produto (title, image, description, price) VALUES ('Mercado Livre', '', 'Descontaço no Mercado Livre ➞ Encontre as ✓ MELHORES OFERTAS ✓ em smartphones, moda, beleza e muito mais!', '79');
+INSERT INTO produto (title, image, description, price) VALUES ('Shopee', '', 'Descubra a plataforma de compras on-line Shopee. Compre em menos de 30 segundos: a qualquer hora e em qualquer lugar. Garantia Shopee | Ofertas Relâmpago.', '0.00');
+INSERT INTO produto (title, image, description, price) VALUES ('KaBum', '', 'Encontre as melhores ofertas em Tecnologia e Games aqui no KaBuM!. Confira nossa linha de Hardware, Smartphones, Computadores, Eletrônicos e mais. Acesse!', '56.77');
+INSERT INTO produto (title, image, description, price) VALUES ('Pichau', '', 'Os Melhores Preços do Brasil é na Pichau! Conheça nossa linha de Computadores Gamer, Cadeiras, Monitores, Placas de Vídeo, Processadores e Muito Mais.', '954.23');
 
--- Codigo SQL aqui
