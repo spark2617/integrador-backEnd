bf55b36bcf9671dbc7ed23e2b91172b84b404a66
Abe <abrahao.184@gmail.com>
2022-09-27 17:36:57 -0300

    CORS

--- src/main/java/br/com/dh/ecommerce/controllers/CategoriaController.java
+++ src/main/java/br/com/dh/ecommerce/controllers/CategoriaController.java
@@ -11,6 +11,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping(value = "/categorias")
 public class CategoriaController {
 
--- src/main/java/br/com/dh/ecommerce/controllers/ProdutoController.java
+++ src/main/java/br/com/dh/ecommerce/controllers/ProdutoController.java
@@ -11,6 +11,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping(value = "/produtos")
 public class ProdutoController {
 
